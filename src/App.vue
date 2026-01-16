73b594f276341fdce5a2c7403bd0c4390ceab7f3
Eric-kx <1156258672@qq.com>
2026-01-16 13:29:02 +0800

    初始提交

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useUserStore } from './stores';
+
+const userStore = useUserStore()
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  {{ userStore.token }}
+  <el-button type="primary" @click="userStore.setToken('dsfsdf')">获取token</el-button>
+  <el-button type="primary" @click="userStore.removeToken()">删除token</el-button>
+</template>
+
+<style scoped></style>
+ 
